9e08ea0eac1e44ec15c6cd92ed444ec4a6e208f5
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-10-27 15:01:36 +0000

    fix: Properties fix

--- src/ApiBureau.Ringover.Api/Dtos/UserDto.cs
+++ src/ApiBureau.Ringover.Api/Dtos/UserDto.cs
@@ -4,10 +4,14 @@ public class UserDto
 {
     [JsonPropertyName("user_id")]
     public required string UserId { get; set; }
-    public required string FirstName { get; set; }
-    public required bool LastName { get; set; }
+
+    [JsonPropertyName("team_id")]
+    public required string TeamId { get; set; }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public required string Email { get; set; }
 
     [JsonPropertyName("concat_name")]
-    public required string ConcatName { get; set; }
+    public string? ConcatName { get; set; }
 }
