89d9d4d377c9edff3e3c3361fd444ea2088c5d8e
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-10-28 16:37:46 +0000

    feat: UserResponse added to a separate file

--- src/ApiBureau.Ringover.Api/Responses/UserResponse.cs
+++ src/ApiBureau.Ringover.Api/Responses/UserResponse.cs
@@ -1,6 +1,6 @@
 namespace ApiBureau.Ringover.Api.Responses;
 
-public class ContactResponse
+public class UserResponse
 {
     [JsonPropertyName("list_count")]
     public int ListCount { get; set; }
