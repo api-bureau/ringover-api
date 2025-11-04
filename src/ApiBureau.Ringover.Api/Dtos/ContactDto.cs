e272fc88ba965d14d40349e55ef6ac137270b5c2
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-04 16:30:23 +0000

    fix: Properties corrected

--- src/ApiBureau.Ringover.Api/Dtos/ContactDto.cs
+++ src/ApiBureau.Ringover.Api/Dtos/ContactDto.cs
@@ -3,7 +3,7 @@ namespace ApiBureau.Ringover.Api.Dtos;
 public class ContactDto
 {
     [JsonPropertyName("contact_id")]
-    public required string ContactId { get; set; }
+    public int? ContactId { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 
@@ -13,8 +13,8 @@ public class ContactDto
     public List<PhoneNumberDto> Numbers { get; set; } = [];
     public string? Note { get; set; }
 
-    [JsonPropertyName("social_profile")]
-    public string? SocialProfile { get; set; }
+    [JsonPropertyName("social_profile_url")]
+    public string? SocialProfileUrl { get; set; }
 
     [JsonPropertyName("social_service")]
     public string? SocialService { get; set; }
--- src/ApiBureau.Ringover.Api/Dtos/PhoneNumberDto.cs
+++ src/ApiBureau.Ringover.Api/Dtos/PhoneNumberDto.cs
@@ -2,6 +2,6 @@ namespace ApiBureau.Ringover.Api.Dtos;
 
 public class PhoneNumberDto
 {
-    public string? Number { get; set; }
+    public long? Number { get; set; }
     public string? Type { get; set; }
 }
